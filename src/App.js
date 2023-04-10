58d2570d2f4586998b86e8dc446eb8e771587c65
Milos Damjanovic <milos.10001@yahoo.com>
2023-04-09 23:47:18 -0400

    checkpoint 6.75, previously added project notes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalLi
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 import './App.css';
 
+/**
+ * 
+ * Logic of the app:
+ *  We add new goals by clicking submit button Add Goal
+ *  We delete goals by just selecting the goal 
+ * 
+ */
+
 const App = () => {
+
+  // setting up DEFAULT VALUES
   const [courseGoals, setCourseGoals] = useState([
     { text: 'Do all exercises!', id: 'g1' },
     { text: 'Finish the course!', id: 'g2' }
   ]);
 
+  // adding Goal functionality, where we add entered goal and update Goal List
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
@@ -18,6 +29,7 @@ const App = () => {
     });
   };
 
+  // function for deleting the goal we selected
   const deleteItemHandler = goalId => {
     setCourseGoals(prevGoals => {
       const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
@@ -29,6 +41,7 @@ const App = () => {
     <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
   );
 
+  // disable the option for user to delete the goal IF there are no items in the component
   if (courseGoals.length > 0) {
     content = (
       <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,30 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
+  // handler for tracking states as we type inside input bar
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
+
+    setEnteredValue(event.target.value); // will pass whatever we type to the input bar
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
+    console.log(`Submiting ${enteredValue} to props.onAddGoal function`);
     props.onAddGoal(enteredValue);
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+    <form onSubmit={formSubmitHandler}> {/* calling formSubmitHandler function once we click submit 'Add Goal' button */}
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}> {/* using template literal and ternary operator, depending on the state we'll add proper class accordingly */}
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
@@ -27,3 +38,13 @@ const CourseInput = props => {
 };
 
 export default CourseInput;
+
+
+
+/* 
+  logic behind passing value to onAddGoal(enteredValue)
+
+  1. We'll use setEnteredValue to track the new state of component WHILE we are typing, as soon as we stop it will be put in enteredValue state
+  2. Once we are satisfied with value that we want to submit, onAddGoal will submit the enteredValue
+
+*/
